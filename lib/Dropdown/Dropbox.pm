be1c4b602a4a61059e22d23ee201b9de7e4fa3da
Yusuke Wada <yusuke@kamawada.com>
2012-02-23 07:34:53 +0900

    fixed css and tidy.

--- lib/Dropdown/Dropbox.pm
+++ lib/Dropdown/Dropbox.pm
@@ -5,28 +5,31 @@ use Encode;
 use Plack::Session;
 
 sub dropbox {
-    my $self          = shift;
-    my $name          = $self->stash->{name};
-    my $dropbox       = $self->app->dropbox;
-    my $session       = Plack::Session->new( $self->req->env );
+    my $self = shift;
+
+    # $nameにはDropboxファイルへのパスが入る
+    my $name = $self->stash->{name};
+    my $dropbox = $self->app->dropbox;
+
+    # sessionにはPlack::Sessionを使用
+    my $session = Plack::Session->new( $self->req->env );
     my $access_token  = $session->get('access_token');
     my $access_secret = $session->get('access_secret');
     return $self->render_not_found if ( !$access_token && !$access_secret );
+
     $dropbox->access_token($access_token);
     $dropbox->access_secret($access_secret);
+
     my $list = $dropbox->metadata($name) or die $dropbox->error;
     $self->stash->{list} = $list;
 
     if ( !$list->{is_dir} ) {
         my $content;
         $dropbox->files(
-            $name,
-            sub {
-
-                # using Furl.
-                $content .= $_[3];
-            }
+            $name,  sub { $content .= $_[3]; } # using Furl.
         );
+
+        # 拡張子がMarkdownのものだったら
         if ( $name =~ m!\.(?:md|mkdn|markdown|mkd|mark|mdown)$! ) {
             $content = decode_utf8($content);
             $self->stash->{markdown_html} = markdown($content);
@@ -34,11 +37,11 @@ sub dropbox {
                 template => 'dropbox/markdown',
                 format   => 'html'
             );
-        }
-        else {
+        } else {
             $self->res->headers->content_type( $list->{mime_type} );
             return $self->render( data => $content );
         }
+
     }
     return $self->render('dropbox/list');
 }
